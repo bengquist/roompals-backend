e6c4cf2abd3962e758bc23fd52da78db9ba0a19e
bengquist <engquist_blake@yahoo.com>
2020-04-19 21:39:13 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { ApolloServer } from "apollo-server";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { ChoreResolver } from "./resolvers/ChoreResolver";
+
+async function main() {
+  // await createConnection();
+  const schema = await buildSchema({
+    resolvers: [ChoreResolver],
+  });
+  const server = new ApolloServer({ schema });
+  await server.listen(4000);
+  console.log("Server has started!");
+}
+
+main();
